97305bea038dccdbbd9fd24f8eecbf3032f3d6d4
Stu Bolderson <stu.bolderson@aol.com>
2022-02-27 11:06:27 +0100

    react starter files removed started creating base layout structure basic setup working correctly - ready to add the atom components and start shaping mobile layout

---
+++ src/screens/Home.jsx
@@ -0,0 +1,10 @@
+import "../components/UI/atoms/Title/index";
+import { Title } from "../components/UI/atoms/Title/index";
+
+export default function Home() {
+  return (
+    <div>
+      <Title text="Welcome to my Portfolio" />
+    </div>
+  );
+}
