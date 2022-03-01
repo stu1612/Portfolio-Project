3c50bf1a56cdbc40d28658e2875c415cfb0d8b06
Stu Bolderson <stu.bolderson@aol.com>
2022-03-01 17:09:11 +0100

    mobile hero completed

--- src/components/templates/Hero/Hero.jsx
+++ src/components/templates/Hero/Hero.jsx
@@ -1,9 +1,16 @@
 import "./style.css";
+import hero from "../../../assets/images/landing-img.jpg";
+import programmer from "../../../assets/images/landing.svg";
+import { Title1 } from "../../UI/atoms/Title/index";
 
 export default function Hero() {
   return (
     <section className="section" id="home">
-      Hero
+      <img src={hero} alt="hello" className="hero-img" />
+      <img src={programmer} alt="programmer" className="programmer" />
+      <div className="overlay">
+        <Title1>Stuart Bolderson</Title1>
+      </div>
     </section>
   );
 }
