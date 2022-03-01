26b915f82d3b3d4e7d7e6792787292aa9425dfd5
Stu Bolderson <stu.bolderson@aol.com>
2022-03-01 19:08:57 +0100

    move on to desktop styling

--- src/components/templates/Contact/Contact.jsx
+++ src/components/templates/Contact/Contact.jsx
@@ -1,10 +1,14 @@
 // atoms
-import { Title2 } from "../../UI/atoms/Title";
+import { Title2, Title3 } from "../../UI/atoms/Title";
 // molecules
 import ScreenShape from "../../UI/molecules/ScreenShape/ScreenShape";
 import TextContent from "../../UI/molecules/TextContent/TextContent";
 // styles
 import "./style.css";
+
+import { AiOutlineMail } from "react-icons/ai";
+import { ImMobile } from "react-icons/im";
+import { ImLocation } from "react-icons/im";
 
 export default function Contact() {
   return (
@@ -12,6 +16,24 @@ export default function Contact() {
       <ScreenShape>
         <Title2>Contact</Title2>
         <TextContent body="Here is my contact information if you are intersted in starting a collaboration !" />
+        <div className="contact-wrapper">
+          <div className="icon-wrapper">
+            <AiOutlineMail size={20} />
+          </div>
+          <Title3>stu.bolderson@aol.com</Title3>
+        </div>
+        <div className="contact-wrapper">
+          <div className="icon-wrapper">
+            <ImMobile size={20} />
+          </div>
+          <Title3>+46 70 857 7935</Title3>
+        </div>
+        <div className="contact-wrapper">
+          <div className="icon-wrapper">
+            <ImLocation size={20} />
+          </div>
+          <Title3>Lund, Sweden</Title3>
+        </div>
       </ScreenShape>
     </section>
   );
--- src/components/templates/Footer/Footer.jsx
+++ src/components/templates/Footer/Footer.jsx
@@ -1,5 +1,19 @@
 import "./style.css";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 
 export default function Footer() {
-  return <footer className="footer-section">Footer</footer>;
+  return (
+    <footer className="footer-section">
+      <div className="footer-links">
+        <div className="wrapper">
+          <FaLinkedin size={28} color="#c6c6c6" />
+        </div>
+        <div className="wrapper">
+          <FaGithub size={28} color="#c6c6c6" />
+        </div>
+      </div>
+      <span>&#169; 2022 * Stu Bolderson</span>
+    </footer>
+  );
 }
